8ffd976ab6f48a4463d1dfa5e3761204e73a7cf6
gustavomagallanesguijon <gustavo.magallanes.guijon@ciencias.unam.mx>
2017-03-23 16:22:12 -0600

    Ya está el script para dos botones: 'Browser' y 'Subir'.

--- upload_zipear.php
+++ upload_zipear.php
@@ -21,17 +21,30 @@ $tipoFile = $_FILES['archivo_zipear']['type'];
                 {
 
                     echo "<script type=\"text/javascript\">alert(\"No se pudo subir su archivo" . $files_zipear . "Error Processing Request\");</script>"; 
-                    
+                   
                 }
         }
-    
-
     else
         {
-
             echo "<script type=\"text/javascript\">alert(\"Este archivo no es válido. Intenta de nuevo.\");</script>";
 
         }
+
+getcwd();
+chdir("files");
+
+                        $nombreSinExtension = reset(explode(".", $files_zipear));
+
+
+$url_enviar='curl -u admin:vS9UI355#ea9 -H "Content-type: application/zip" -T ${TRAINING_ROOT}/var/www/html/metadataCapture/files/'. $files_zipear . ' http://ssig0.conabio.gob.mx:8080/geoserver/rest/workspaces/myworkspace/datastores/' . $nombreSinExtension . '/file.shp';
+
+shell_exec($url_enviar);
+
+$mover_zip='mv '.$files_zipear . ' basura/';
+
+shell_exec($mover_zip);
+
+echo "<script type=\"text/javascript\">alert(\"Se han enviado los archivos a la CONABIO\");</script>"; 
 
 ?>
 
