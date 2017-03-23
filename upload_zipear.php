8a277b8cd1c87e68320d70a6160a8a157580d8cf
gustavomagallanesguijon <gustavo.magallanes.guijon@ciencias.unam.mx>
2017-03-23 17:47:46 -0600

    Ya no están los botones de shp, shs, dbf y prj. Ni sus archivos php

--- upload_zipear.php
+++ upload_zipear.php
@@ -35,7 +35,6 @@ chdir("files");
 
                         $nombreSinExtension = reset(explode(".", $files_zipear));
 
-
 $url_enviar='curl -u admin:vS9UI355#ea9 -H "Content-type: application/zip" -T ${TRAINING_ROOT}/var/www/html/metadataCapture/files/'. $files_zipear . ' http://ssig0.conabio.gob.mx:8080/geoserver/rest/workspaces/myworkspace/datastores/' . $nombreSinExtension . '/file.shp';
 
 shell_exec($url_enviar);
